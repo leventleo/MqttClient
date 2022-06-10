13f598d221e35273b61ee78875111a05ef327693
levent19782003 <levent19782003@gmail.com>
2022-06-10 21:17:30 +0300

    Add project files.

---
+++ MqttClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MqttClient
+{
+    class Program
+    {
+        static async  Task Main(string[] args)
+        {
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder()
+                    .WithTcpServer("127.0.0.1")
+                    .Build();
+
+                 await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                var applicationMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic("samples/temperature/living_room")
+                    .WithPayload("19.5")
+                    .Build();
+
+                  await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
+                Console.WriteLine("MQTT application message is published.");
+            }
+
+        }
+    }
+ 
+}
---
+++ MqttConsumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MqttConsumer
+{
+    class Program
+    {
+        static async  Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder()
+                    .WithTcpServer("127.0.0.1")
+                    .Build();
+
+                // Setup message handling before connecting so that queued messages
+                // are also handled properly. When there is no event handler attached all
+                // received messages get lost.
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine("Received application message.");
+                  
+
+                    return Task.CompletedTask;
+                };
+
+                 await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                    .WithTopicFilter(f => { f.WithTopic("samples/temperature/living_room"); })
+                    .Build();
+
+                await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+                Console.WriteLine("MQTT client subscribed to topic.");
+
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
